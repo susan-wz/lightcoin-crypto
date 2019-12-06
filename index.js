1bce993e87a8ce1fa39f2f8ceff552c82e3007a2
Susan Zhang <susanwenzhang@gmail.com>
2019-12-06 21:08:39 +0000

    completed feature 1 but not missing feature 2 yet

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    let balance = 0;
+    for (let i = 0; i < this.transactions.length; i++) {
+      console.log(this.transactions[i]);
+      balance += this.transactions[i].amount;
+    } return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
-  }
+    this.time = new Date();
+    this.account.addTransaction(this);
 
+  }
 }
 
+class Withdrawal extends Transaction {
 
+  get value() {
+    return -(this.amount);
+  }
+}
 
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account("susanz");
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Transaction 1:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 2:', t3);
+
+console.log('Balance:', myAccount.balance);
